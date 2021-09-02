7009325335b951bf2d7a83a4315b230346acbc59
Johirul Islam Nishat <jahirulislamn@gmail.com>
2021-09-02 22:25:18 +0600

    final update

--- js/main.js
+++ js/main.js
@@ -11,33 +11,70 @@ const inputBtn = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => displayBookList(data.docs))
+        //.then(data => console.log(data))
+        .then(data => displayBookList(data))
+
 
 }
 
 const displayBookList = bookData => {
+
+    const booksInfo = bookData.docs;
+
+    //error id finding
+    const displayError = document.getElementById('error');
+    displayError.textContent = '';
+
+    //totalcount id finding
+    const totalBookCount = document.getElementById('totalCount');
+    totalBookCount.textContent = '';
+
+    //booklist id finding
     const bookListId = document.getElementById('bookListId');
     bookListId.textContent = '';
 
-    bookData.forEach(bookList => {
-        // console.log(bookList)
+    //error handling
+    if (bookData.numFound === 0 || bookData.numFound === '') {
 
-        const bookListDiv = document.createElement('div');
-        bookListDiv.classList.add('col');
-        bookListDiv.innerHTML = `
-        <div class="card h-100">
-        <img src="https://covers.openlibrary.org/b/id/${bookList.cover_i}-M.jpg" class="card-img-top h-50" alt="...">
-            <div class="card-body mt-2">
-            
-            <h5><i class="fas fa-book icon"></i> <span class="cardText">${bookList.title}</span></h5>
-            
-            <h5><i class="fas fa-user icon"></i> <span class="cardText">${bookList.author_name}</span></h4>
-            
-            <h5><i class="fas fa-clock icon"></i> <span class="cardText">${bookList.publish_year}</span></h5>
+        // const displayError = document.getElementById('error');
+        // displayError.textContent = '';
 
-            </div>
-    </div>
-`
-        bookListId.appendChild(bookListDiv);
-    })
+        displayError.innerHTML = `
+        <h1 class="text-center text-danger  mx-auto" >Ohhhhh! Please enter valid Book Name</h1>
+`;
+
+    }
+    else {
+
+        //total count
+        totalBookCount.innerHTML = `
+
+        <div class="card-body mt-2">
+            <h4 class="text-center">Total: <span class="cardText"> ${booksInfo.length}</span> Out Of: <span class="cardText"> ${bookData.numFound}</span></h4>
+
+                </div>
+        `
+
+        // show booklist
+
+        booksInfo.forEach(bookList => {
+            const bookListDiv = document.createElement('div');
+            bookListDiv.classList.add('col');
+            bookListDiv.innerHTML = `
+            <div class="card h-100">
+            <img src="https://covers.openlibrary.org/b/id/${bookList.cover_i}-M.jpg" class="card-img-top h-50" alt="...">
+                <div class="card-body mt-2">
+
+                <h5><i class="fas fa-book icon"></i> <span class="cardText">${bookList.title}</span></h5>
+
+                <h5><i class="fas fa-user icon"></i> <span class="cardText">${bookList.author_name}</span></h4>
+
+                <h5><i class="fas fa-clock icon"></i> <span class="cardText">${bookList.publish_year}</span></h5>
+
+                </div>
+        </div>
+    `
+            bookListId.appendChild(bookListDiv);
+        })
+    }
 }
